5485d0800634ea2eda3f9b107a923bd4975b1ca0
Gloriazira <gloriazira9@gmail.com>
2020-10-02 12:59:52 +0100

    correcting js file

--- calculator.js
+++ calculator.js
@@ -1,30 +1,30 @@
 function ageCalculate(){
     
-   var birthDate =document.getElementById('date').value;
-     var name =document.getElementById('name').value;
+    var birthDate =document.getElementById('date').value;
+      var name =document.getElementById('name').value;
+  
+  
+       
+         var myName = name.toString();
+         var yourAge = parseInt(birthDate);
  
- 
-      
-        var myName = name.toString();
-        var yourAge = parseInt(birthDate);
-
-        days_lived = yourAge * 365;
-         
-         
-          if (isNaN(yourAge)) {
-              document.getElementById("result").innerHTML = (" You've entered an Invalid age");
-          }
-         else {
-             document.getElementById("result").innerHTML = "My name is "+ myName + 
-             "<br/><br/> And below is my age breakdown: <br/>" + days_lived + " days lived " ;
-            
-         
-         if (yourAge < 20) {
-            document.getElementById("message").innerHTML = ("You are Under-age");
-        }
-        else{
-            document.getElementById("message").innerHTML = ("You are an Adult");
-        }
-    }
- 
- }
+         days_lived = yourAge * 365;
+          
+          
+           if (isNaN(yourAge)) {
+               document.getElementById("result").innerHTML = (" You've entered an Invalid age");
+           }
+          else {
+              document.getElementById("result").innerHTML = "My name is "+ myName + "<br/>I am " + yourAge + "years old" + 
+              "<br/><br/> Here's my age in days: <br/>" + days_lived + " days lived " ;
+             
+          
+          if (yourAge < 20) {
+             document.getElementById("message").innerHTML = ("You are Under-age");
+         }
+         else{
+             document.getElementById("message").innerHTML = ("You are an Adult");
+         }
+     }
+  
+  }
